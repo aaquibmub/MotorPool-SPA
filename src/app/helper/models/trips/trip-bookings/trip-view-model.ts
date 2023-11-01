95049403e53eb6a93d4be8d013dd1c963581c85e
Abdul Wahab <abdulwahab@afroatlas.com>
2023-11-01 23:08:11 +0500

    trip view model and view updates

---
+++ src/app/helper/models/trips/trip-bookings/trip-view-model.ts
@@ -0,0 +1,34 @@
+import { DropdownItem } from '../../common/dropdown/dropdown-item.model';
+import { TripDestinationModel } from '../enroute/trip-destination-model';
+import { Gender, TripDestination, TripRoute } from './../../../common/shared-types';
+import { TripBookingPassengerModel } from './trip-booking-passenger-model';
+import { TripBookingSpecialServiceModel } from './trip-booking-special-service-model';
+
+export class TripViewBooking {
+  id: string;
+  serialNumber: number;
+  bookedBy: DropdownItem<string>;
+  approvedBy: DropdownItem<string>;
+
+  requester: DropdownItem<string>;
+  requesterGender: DropdownItem<Gender>;
+  requesterAddress: DropdownItem<string>;
+  isRequesterTraveling: boolean;
+  passengers: TripBookingPassengerModel[];
+
+  isSpecialServicesRequired: boolean;
+  specialSevices: TripBookingSpecialServiceModel[];
+
+  tripRoute: DropdownItem<TripRoute>;
+  tripDestination: DropdownItem<TripDestination>;
+
+  startingPoint: DropdownItem<string>;
+  destinations: TripDestinationModel[];
+
+  driver: DropdownItem<string>;
+  vehical: DropdownItem<string>;
+  registrationPlate: string;
+
+  notes: string;
+
+}
