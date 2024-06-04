4c2a3191664b62ac987802df148196e28b136e76
Aaquib Mubarak <aaquib.mubarak@gmail.com>
2024-06-04 15:07:22 +0500

    changes

--- src/app/modules/passenger/components/passenger-list/passengers-all/passengers-all.component.ts
+++ src/app/modules/passenger/components/passenger-list/passengers-all/passengers-all.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataStateChangeEvent, GridDataResult } from '@progress/kendo-angular-grid';
+import { flatten } from '@progress/kendo-angular-grid/dist/es2015/filtering/base-filter-cell.component';
 import { State } from '@progress/kendo-data-query';
 import { Subscription } from 'rxjs';
+import { Gender, GetGenderForDropdownList, GetOpmForDropdownList, OPM } from 'src/app/helper/common/shared-types';
 import { UtilityRix } from 'src/app/helper/common/utility-rix';
+import { DropdownItem } from 'src/app/helper/models/common/dropdown/dropdown-item.model';
 import { ActionButton } from 'src/app/helper/models/common/grid/action-button';
 import { PassangerGridModel } from 'src/app/helper/models/passengers/passanger-grid-model';
 import { GridToolbarService } from 'src/app/helper/services/common/grid-toolbar.service';
 import { UtilityService } from 'src/app/helper/services/common/utility.service';
 import { PassangerService } from 'src/app/helper/services/passanger/passanger.service';
+import { AgeGroupService } from 'src/app/helper/services/utilities/age-group.service';
 
 @Component({
   selector: 'app-passengers-all',
@@ -23,17 +27,33 @@ export class PassengersAllComponent implements OnInit {
   filterable = UtilityRix.gridConfig.filterable;
   searchQuery: string;
 
+  genderList: DropdownItem<Gender>[] = GetGenderForDropdownList();
+  selectedGender: DropdownItem<Gender>;
+
+  ageGroupList: DropdownItem<string>[] = [];
+  selectedAgeGroup: DropdownItem<string>;
+
+  opmList: DropdownItem<OPM>[] = GetOpmForDropdownList();
+  selectedOpm: DropdownItem<OPM>;
+
   pageSizeSubscription: Subscription;
   gridFilterSubscription: Subscription;
 
   constructor(
     public utilityService: UtilityService,
     private passangerService: PassangerService,
+    private ageGroupService: AgeGroupService,
     private router: Router,
     private gridToolbarService: GridToolbarService
   ) { }
 
   ngOnInit(): void {
+
+    this.ageGroupService.getDropdownList('')
+      .subscribe((list: DropdownItem<string>[]) => {
+        this.ageGroupList = list;
+      });
+
     this.gridFilterSubscription = this.gridToolbarService.getGridFilter()
       .subscribe(
         (show: boolean) => {
@@ -64,6 +84,58 @@ export class PassengersAllComponent implements OnInit {
         }
       );
   }
+
+  handleGenderValueChange(value: DropdownItem<Gender>): void {
+    const root = { logic: 'and', filters: [], ...this.state.filter };
+    const [filter] = flatten(root).filter(x => x.field === "gender");
+    if (!filter) {
+      root.filters.push({
+        field: "gender",
+        operator: "eq",
+        value: value.value
+      });
+    } else {
+      filter.value = value.value;
+    }
+    this.selectedGender = value;
+    this.state.filter = root;
+    this.dataStateChange(this.state as DataStateChangeEvent);
+  }
+
+  handleAgeGroupValueChange(value: DropdownItem<string>): void {
+    const root = { logic: 'and', filters: [], ...this.state.filter };
+    const [filter] = flatten(root).filter(x => x.field === "ageGroupId");
+    if (!filter) {
+      root.filters.push({
+        field: "ageGroupId",
+        operator: "eq",
+        value: value.value
+      });
+    } else {
+      filter.value = value.value;
+    }
+    this.selectedAgeGroup = value;
+    this.state.filter = root;
+    this.dataStateChange(this.state as DataStateChangeEvent);
+  }
+
+  handleOpmValueChange(value: DropdownItem<OPM>): void {
+    const root = { logic: 'and', filters: [], ...this.state.filter };
+    const [filter] = flatten(root).filter(x => x.field === "opm");
+    if (!filter) {
+      root.filters.push({
+        field: "opm",
+        operator: "eq",
+        value: value.value
+      });
+    } else {
+      filter.value = value.value;
+    }
+    this.selectedOpm = value;
+    this.state.filter = root;
+    this.dataStateChange(this.state as DataStateChangeEvent);
+  }
+
 
   dataStateChange(state: DataStateChangeEvent): void {
     this.state = state;
